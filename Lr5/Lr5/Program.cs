ee79593009d055a9c6cc188cf9ed669e73f59c83
Frundsberg <114298911+Frundsberg@users.noreply.github.com>
2023-03-23 21:32:52 +0200

    Add files via upload

---
+++ Lr5/Lr5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Зчитуємо послідовність символів з консолі
+                Console.Write("Введіть послідовність символів з зайвими пропусками: ");
+                string input = Console.ReadLine();
+                // Замінюємо пропуски спочатку розбиваючи введену послідовність
+                // з пробілом у якості роздільника а потім об'єднуємо заново
+                string output = string.Join(" ", input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                // Виводимо результат на консоль
+                Console.WriteLine(output);
+                Console.ReadLine();
+            }
+        }
+         
+
+
+    }
+}
+
