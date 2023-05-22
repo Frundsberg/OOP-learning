65d82ef9aff654f6c478e3ab44334bb8d8fb30a3
Frundsberg <114298911+Frundsberg@users.noreply.github.com>
2023-05-22 21:26:54 +0300

    Add files via upload

---
+++ LAB17/LAB17/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace LAB17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(175, 31);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 27);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Приєднатись";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(172, 305);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(77, 42);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Відправити";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(93, 67);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 2;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(9, 84);
+            this.txtChat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(254, 217);
+            this.txtChat.TabIndex = 3;
+            this.txtChat.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(9, 305);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(160, 43);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.Text = "";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(9, 38);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(160, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(5, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ім\'я користувача:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Статус мережі:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(277, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "ChatClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LAB17/LAB17/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LAB17
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client; // Клас для взаємодії з TCP-клієнтом
+        private NetworkStream stream; // Потік для зчитування та запису даних через мережу
+        private Thread receiveThread; // Потік для отримання повідомлень від сервера
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обробник натиснення кнопки "Підключитись"
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string userName = txtUsername.Text;
+            client = new TcpClient();
+
+            try
+            {
+                // Встановлення з'єднання з сервером
+                client.Connect("127.0.0.1", 8888);
+                stream = client.GetStream();
+                byte[] data = Encoding.Unicode.GetBytes(userName);
+                stream.Write(data, 0, data.Length);
+
+                // Створення та запуск потоку для отримання повідомлень від сервера
+                receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start();
+
+                lblStatus.Text = "Підключено";
+                btnSend.Enabled = true;
+                btnConnect.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка підключення до сервера: " + ex.Message);
+                Disconnect();
+            }
+        }
+
+        // Обробник натиснення кнопки "Відправити"
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            txtMessage.Clear();
+
+            // Додавання відправленого повідомлення до текстового поля чату
+            DisplayMessage("Я: " + message);
+        }
+
+        // Метод для отримання повідомлень від сервера
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    // Зчитування даних з потоку, поки вони доступні
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    DisplayMessage(message);
+                }
+                catch
+                {
+                    DisplayMessage("Підключення перервано!");
+                    Disconnect();
+                    break;
+                }
+            }
+        }
+
+        // Метод для відображення повідомлення в текстовому полі чату
+        private void DisplayMessage(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(DisplayMessage), message);
+            }
+            else
+            {
+                txtChat.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        // Обробник події закриття форми
+        private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        // Метод для відключення від сервера
+        private void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+
+            if (receiveThread != null && receiveThread.IsAlive)
+                receiveThread.Join();
+
+            lblStatus.Text = "Відключено";
+            btnSend.Enabled = false;
+            btnConnect.Enabled = true;
+        }
+    }
+}
