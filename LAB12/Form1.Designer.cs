aecec955edab674a81c91c015f3196e0c9db5ce1
Frundsberg <114298911+Frundsberg@users.noreply.github.com>
2023-05-22 21:24:40 +0300

    Add files via upload

---
+++ LAB12/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace LAB12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.nazvaTextBox = new System.Windows.Forms.TextBox();
+            this.opusTextBox = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(10, 159);
+            this.GenerateButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(226, 41);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Сгенерувати";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // nazvaTextBox
+            // 
+            this.nazvaTextBox.Location = new System.Drawing.Point(66, 10);
+            this.nazvaTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nazvaTextBox.Name = "nazvaTextBox";
+            this.nazvaTextBox.Size = new System.Drawing.Size(170, 20);
+            this.nazvaTextBox.TabIndex = 1;
+            // 
+            // opusTextBox
+            // 
+            this.opusTextBox.Location = new System.Drawing.Point(66, 48);
+            this.opusTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.opusTextBox.Name = "opusTextBox";
+            this.opusTextBox.Size = new System.Drawing.Size(170, 64);
+            this.opusTextBox.TabIndex = 7;
+            this.opusTextBox.Text = "";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(66, 117);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(170, 20);
+            this.dateTimePicker.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Кому";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Від кого";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Дата";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 258);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.opusTextBox);
+            this.Controls.Add(this.nazvaTextBox);
+            this.Controls.Add(this.GenerateButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "LAB12";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox nazvaTextBox;
+        private System.Windows.Forms.RichTextBox opusTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LAB12/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+namespace LAB12
+{
+    public partial class Form1 : Form
+    {
+        private string templatePath = "C:\\Users\\User\\Desktop\\Gift.docx";
+        private string outputPath = "C:\\Users\\User\\Desktop\\NewDocument.docx";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Створення об'єкту додатку Microsoft Word
+                var wordApp = new Application();
+
+                // Відкриття шаблону документа
+                var document = wordApp.Documents.Open(templatePath);
+
+                // Отримання секцій документа
+                var sections = document.Sections;
+                foreach (Section section in sections)
+                {
+                    // Отримання назви секції
+                    var sectionName = section.Range.Text;
+
+                    // Виведення назви секції
+                    Console.WriteLine("Назва секції: " + sectionName);
+                }
+                
+                // Застосування різних шаблонів до секцій документа
+                foreach (Section section in sections)
+                {
+                    ApplyTemplateToSection(section);
+                }
+                // Заміна тексту в шаблоні 1 на введений користувачем текст
+                var range = document.Content;
+                range.Find.Execute(FindText: "Кому:", ReplaceWith: nazvaTextBox.Text);
+
+                var range1 = document.Content;
+                range1.Find.Execute(FindText: "От:", ReplaceWith: opusTextBox.Text);
+
+                var range2 = document.Content;
+                range2.Find.Execute(FindText: "Дата выпуска:", ReplaceWith: dateTimePicker.Text);
+
+                // Заміна тексту в шаблоні 2 на введений користувачем текст
+                var range6 = document.Content;
+                range6.Find.Execute(FindText: "Кому2:", ReplaceWith: nazvaTextBox.Text);
+
+                var range7 = document.Content;
+                range7.Find.Execute(FindText: "От2:", ReplaceWith: opusTextBox.Text);
+
+                var range8 = document.Content;
+                range8.Find.Execute(FindText: "Дата выпуска2:", ReplaceWith: dateTimePicker.Text);
+
+                
+
+          
+                
+                // Збереження згенерованого документа
+                document.SaveAs(outputPath);
+
+                // Закриття документа та додатку Microsoft Word
+                document.Close();
+                wordApp.Quit();
+
+                MessageBox.Show("Документ згенеровано успішно.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Сталася помилка при генерації документа: " + ex.Message);
+            }
+        }
+
+    }
+}
